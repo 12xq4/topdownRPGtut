d427a01b4b6f7f4b3c2fba151074ec456a8106b2
12xq4 <12xq4@queensu.ca>
2015-12-23 22:26:07 -0600

    video 1 completed

---
+++ new_rpg/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+    Vector3 position;
+    public float speed;
+    CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = transform.GetComponent<CharacterController>();
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0))
+        {
+            // find where the player clicked
+            LocatePosition();
+        }
+        // move the player
+        ClickAndMove();
+
+    }
+
+    public void LocatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Debug.Log(position);
+        }
+    }
+
+    public void ClickAndMove()
+    {
+        if (Vector3.Distance(transform.position, position) > 1)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(position - transform.position);
+            newRotation.x = 0;
+            newRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            cc.SimpleMove(transform.forward * speed);
+        }
+    }
+}
